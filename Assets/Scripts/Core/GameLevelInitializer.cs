a0eb4c61ce75f8bdf651adabfd8a1a36529d001a
Ivan Diordiev <i.diordev@gmail.com>
2023-03-28 21:56:02 +0300

    add ui controls

---
+++ Assets/Scripts/Core/GameLevelInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+namespace Core
+{
+    public class GameLevelInitializer : MonoBehaviour
+    {
+        [SerializeField] private PlayerEntity _playerEntity;
+        [SerializeField] private GameUIInputView _gameUIInputView;
+
+        private ExternalDevicesInputReader _externalDevicesInputReader;
+        private PlayerBrain _playerBrain;
+
+        private bool _onPause = false;
+            
+        private void Awake()
+        {
+            _externalDevicesInputReader = new ExternalDevicesInputReader();
+            _playerBrain = new PlayerBrain(_playerEntity,
+                new List<IEntityInputSource>() { _gameUIInputView, _externalDevicesInputReader });
+        }
+
+        private void Update()
+        {
+            if(_onPause)
+                return;
+            
+            _externalDevicesInputReader.OnUpdate();
+        }
+
+        private void FixedUpdate()
+        {
+            if(_onPause)
+                return;
+            
+            _playerBrain.OnFixedUpdate();
+        }
+    }
+}
